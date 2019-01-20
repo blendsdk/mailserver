3ee4443be67b9636f98b7ad47291cae0d2530de0
Gevik Babakhani <gevik@blendjs.com>
2019-01-20 22:10:10 +0100

    Replaced mysql with postgresql

--- install.sh
+++ install.sh
@@ -12,27 +12,10 @@ fi
 # force set home to root
 export HOME=/root
 
-echo "Installing MySQL"
-command apt-get -qq install mysql-server -y 2>/dev/null || {
-  echo >&2 'Failed to install MySQL!'
+echo "Installing PostgreSQL"
+command apt-get -qq install postgresql -y 2>/dev/null || {
+  echo >&2 'Failed to install PostgreSQL!'
   exit 2
 }
-
-echo "Starting MySQL"
-command service mysql restart 2> /dev/null || {
-  echo >&2 'Failed to start MySQL!'
-  exit 2
-}
-
-echo "Configuring MySQL"
-command echo -e "y\n" | mysql_secure_installation || {
-  echo >&2 'Failed to configure MySQL!'
-  exit 2
-}
-
-# cleanup
-# rm -v ~/secure_our_mysql.sh
-
-echo "==> MySQL Password is: ${MYSQL_PASSWORD}"
 
 } # this ensures the entire script is downloaded #
