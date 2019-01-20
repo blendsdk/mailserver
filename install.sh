15619105cb46a2eaafac0e668f912f2667a5bf4b
Gevik Babakhani <gevik@blendjs.com>
2019-01-20 21:27:52 +0100

    set mysql option

--- install.sh
+++ install.sh
@@ -30,37 +30,8 @@ command service mysql restart 2> /dev/null || {
   exit 2
 }
 
-tee ~/secure_our_mysql.sh > /dev/null << EOF
-spawn $(which mysql_secure_installation)
-
-expect "Enter password for user root:"
-send "$MYSQL_ROOT_PASSWORD\r"
-
-expect "Press y|Y for Yes, any other key for No:"
-send "y\r"
-
-expect "Please enter 0 = LOW, 1 = MEDIUM and 2 = STRONG:"
-send "2\r"
-
-expect "Change the password for root ? ((Press y|Y for Yes, any other key for No) :"
-send "n\r"
-
-expect "Remove anonymous users? (Press y|Y for Yes, any other key for No) :"
-send "y\r"
-
-expect "Disallow root login remotely? (Press y|Y for Yes, any other key for No) :"
-send "y\r"
-
-expect "Remove test database and access to it? (Press y|Y for Yes, any other key for No) :"
-send "y\r"
-
-expect "Reload privilege tables now? (Press y|Y for Yes, any other key for No) :"
-send "y\r"
-
-EOF
-
 echo "Configuring MySQL"
-command expect ~/secure_our_mysql.sh || {
+command echo -e "y\n" | mysql_secure_installation || {
   echo >&2 'Failed to configure MySQL!'
   exit 2
 }
