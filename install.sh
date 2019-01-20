7d80b2eac85b7add1b565743988c13826b367317
Gevik Babakhani <gevik@blendjs.com>
2019-01-20 21:18:51 +0100

    Type to secure mysql

--- install.sh
+++ install.sh
@@ -12,8 +12,14 @@ fi
 # force set home to root
 export HOME=/root
 
+echo "Installing Expect"
+command apt-get -qq install expect > /dev/null || {
+   echo >&2 'Failed to install Expect!'
+  exit 2
+}
+
 echo "Installing MySQL"
-command apt-get install mysql-server -y 2>/dev/null || {
+command apt-get -qq install mysql-server -y 2>/dev/null || {
   echo >&2 'Failed to install MySQL!'
   exit 2
 }
@@ -24,12 +30,43 @@ command service mysql restart 2> /dev/null || {
   exit 2
 }
 
-# securing mysql
-command mysql -e "UPDATE mysql.user SET Password=PASSWORD('${MYSQL_PASSWORD}') WHERE User='root';"
-mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
-mysql -e "DELETE FROM mysql.user WHERE User='';"
-mysql -e "DROP DATABASE test;"
-mysql -e "FLUSH PRIVILEGES;"
+tee ~/secure_our_mysql.sh > /dev/null << EOF
+spawn $(which mysql_secure_installation)
+
+expect "Enter password for user root:"
+send "$MYSQL_ROOT_PASSWORD\r"
+
+expect "Press y|Y for Yes, any other key for No:"
+send "y\r"
+
+expect "Please enter 0 = LOW, 1 = MEDIUM and 2 = STRONG:"
+send "2\r"
+
+expect "Change the password for root ? ((Press y|Y for Yes, any other key for No) :"
+send "n\r"
+
+expect "Remove anonymous users? (Press y|Y for Yes, any other key for No) :"
+send "y\r"
+
+expect "Disallow root login remotely? (Press y|Y for Yes, any other key for No) :"
+send "y\r"
+
+expect "Remove test database and access to it? (Press y|Y for Yes, any other key for No) :"
+send "y\r"
+
+expect "Reload privilege tables now? (Press y|Y for Yes, any other key for No) :"
+send "y\r"
+
+EOF
+
+echo "Configuring MySQL"
+command expect ~/secure_our_mysql.sh || {
+  echo >&2 'Failed to configure MySQL!'
+  exit 2
+}
+
+# cleanup
+# rm -v ~/secure_our_mysql.sh
 
 echo "==> MySQL Password is: ${MYSQL_PASSWORD}"
 
