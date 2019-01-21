c26e714a2af659396127df2ba087ed0c4c0e0b89
Gevik Babakhani <gevik@blendjs.com>
2019-01-22 00:15:34 +0100

    Refactored progress

--- install.sh
+++ install.sh
@@ -37,36 +37,31 @@ fi
 
 export MAILSERVER_FDQN
 
-echo -ne "\e[93mPreparing..."
+echo -ne "\e[93mPreparing"
 apt-get update -y >> /dev/null 2>&1
 apt-get upgrade -y >> /dev/null 2>&1
-echo -e ",\e[96mdone."
 
 if ! [ -x "$(which php)" ]; then
-    echo -ne "\e[93mInstalling PHP:..."
     apt-get install php-cli -y >> /dev/null 2>&1
-    echo -e ",\e[96mdone."
+    echo -e "."
 fi
 
 if ! [ -x "$(which composer)" ]; then
-    echo -ne "\e[93mInstalling Composer:..." && \
     php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" >> /dev/null && \
     php composer-setup.php --install-dir=/usr/local/bin --filename=composer >> /dev/null && \
     php -r "unlink('composer-setup.php');" && \
-    echo -e ",\e[96mdone."
+    echo -e "."
 fi
 
 if ! [ -x "$(which git)" ]; then
-    echo -ne "\e[93mInstalling GIT:..." && \
     apt-get install git -y >> /dev/null 2>&1
-    echo -e ",\e[96mdone."
+    echo -e "."
 fi
 
 if [ -d "./mailserver" ]; then
     rm -fR ./mailserver
 fi
 
-echo -ne "\e[93mGetting setup files..."
 git clone --depth=1 https://github.com/blendsdk/mailserver.git >> /dev/null 2>&1
 cd mailserver
 composer update >> /dev/null 2>&1
