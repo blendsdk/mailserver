90d4d67793f2a86a86b89b0166fa089d798886ad
Gevik Babakhani <gevik@blendjs.com>
2019-01-21 21:42:19 +0100

    removed deprecated files

--- postgresql.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-echo -ne "\e[93mInstalling PostgreSQL..."
-apt-get install postgresql -y >> /dev/null 2>&1
-sudo -u postgres psql -c "create role ${MAIL_USER} with login password '${DB_PASSWORD}';" >> /dev/null 2>&1
-sudo -u postgres psql -c "create database ${MAILSERVER_FDQN} owner ${MAIL_USER};" >> /dev/null 2>&1
-echo -e "\e[93mdone"
-exit $?
