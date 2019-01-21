9415c4405073f0c92efa224288552465bb33e95b
Gevik Babakhani <gevik@blendjs.com>
2019-01-21 21:00:50 +0100

    Fixed logging

--- postgresql.sh
+++ postgresql.sh
@@ -4,5 +4,5 @@ echo -ne "\e[93mInstalling PostgreSQL..."
 apt-get install postgresql -y >> /dev/null 2>&1
 sudo -u postgres psql -c "create role ${MAIL_USER} with login password '${DB_PASSWORD}';" >> /dev/null 2>&1
 sudo -u postgres psql -c "create database ${MAILSERVER_FDQN} owner ${MAIL_USER};" >> /dev/null 2>&1
-echo -ne "\e[93mdone"
+echo -e "\e[93mdone"
 exit $?
