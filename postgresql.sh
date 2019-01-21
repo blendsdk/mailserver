3f13945113466a1b1a0e3568ed5c557c074c3909
Gevik Babakhani <gevik@blendjs.com>
2019-01-21 21:00:00 +0100

    fixed db creation

--- postgresql.sh
+++ postgresql.sh
@@ -2,7 +2,7 @@
 
 echo -ne "\e[93mInstalling PostgreSQL..."
 apt-get install postgresql -y >> /dev/null 2>&1
-sudo -u postgres psql -c "create role mail with login password '${DB_PASSWORD}';" >> /dev/null 2>&1
-sudo -u postgres psql -c "create database mailserver owner mail;" >> /dev/null 2>&1
+sudo -u postgres psql -c "create role ${MAIL_USER} with login password '${DB_PASSWORD}';" >> /dev/null 2>&1
+sudo -u postgres psql -c "create database ${MAILSERVER_FDQN} owner ${MAIL_USER};" >> /dev/null 2>&1
 echo -ne "\e[93mdone"
 exit $?
