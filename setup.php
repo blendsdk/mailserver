66cb44debe78697dc605933949c9e1da4772020e
Gevik Babakhani <gevik@blendjs.com>
2019-01-21 23:07:25 +0100

    installing postgresql

--- setup.php
+++ setup.php
@@ -3,7 +3,25 @@
 require_once dirname(__FILE__) . '/vendor/autoload.php';
 
 use Colors\Color;
-$c = new Color();
+$color = new Color();
 
-echo $c(getenv('MAILSERVER_FDQN'))->white()->bold()->highlight('green') . PHP_EOL;
+function prompt_info($message) {
+    echo $color($message)->yellow;
+}
+
+function prompt_done() {
+    echo $color('done.')->cyan . PHP_EOL;
+}
+
+function install_system_package(array $packages) {
+    shell_exec("apt-get install -y " . implode(" ", $packages) . " /dev/null 2>&1");
+}
+
+function install_postgresql() {
+    prompt_info("Installing PostgreSQL...");
+    install_system_package(["postgresql"]);
+    prompt_done();
+}
+
+install_postgresql();
 
